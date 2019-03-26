6fa62d4fb27497828588fa15d1381274c5b67f63
mktosos <taniguchidesigns@hotmail.com>
2019-03-26 16:01:31 -0700

    config routes set, server working without errors, initial html content added

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,22 @@
+// Use path package for html routing
+var path = require("path");
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // Direct user to html page requested via GET request
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
+
+  // If no matching route is found default to home
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+};
