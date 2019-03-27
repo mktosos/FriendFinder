77da22bc488357b4bb0fa105be7fbfbb8a89f384
mktosos <taniguchidesigns@hotmail.com>
2019-03-26 17:58:03 -0700

    friends array data and module.exports added

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,31 +1,12 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
-
+//  linking routes to friends data array 
 var friendsData = require("../data/friends");
-//var waitListData = require("../data/waitinglistData");
 
-
-// ===============================================================================
 // ROUTING
-// ===============================================================================
-
 module.exports = function(app) {
-    // API GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases when a user visits a link
-    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-    // ---------------------------------------------------------------------------
-  
+    // handles API GET Requests for data, presented as JSON
     app.get("/api/friends", function(req, res) {
       res.json(friendsData);
     });
-  
-    // app.get("/api/waitlist", function(req, res) {
-    //   res.json(waitListData);
-    // });
   
     // API POST Requests
     // Below code handles when a user submits a form and thus submits data to the server.
