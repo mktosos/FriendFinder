8c93f44b35e3f5b50c598fb4b0077adbaa19273f
mktosos <taniguchidesigns@hotmail.com>
2019-03-27 23:28:52 -0700

    post route added

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -16,19 +16,19 @@ module.exports = function(app) {
     // Then the server saves the data to the tableData array)
     // ---------------------------------------------------------------------------
   
-    // app.post("/api/tables", function(req, res) {
+    app.post("/api/friends", function(req, res) {
     //   // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     //   // It will do this by sending out the value "true" have a table
     //   // req.body is available since we're using the body parsing middleware
     //   if (tableData.length < 5) {
-    //     tableData.push(req.body);
-    //     res.json(true);
+        friendsData.push(req.body);
+        res.json(true);
     //   }
     //   else {
     //     waitListData.push(req.body);
     //     res.json(false);
     //   }
-    // });
+    });
   
     // ---------------------------------------------------------------------------
     // I added this below code so you could clear out the table while working with the functionality.
