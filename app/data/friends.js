41d4ed09502d78c275d668d33a0bf1b2b5ab718a
mktosos <taniguchidesigns@hotmail.com>
2019-03-29 22:22:10 -0700

    array format issue resolved

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,19 @@
 var friendsArray= [
     {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-    }
+        "name": "Syd",
+        "photo": "https://imgur.com/gallery/E3pam",
+        "scores": [
+        "3",
+        "2",
+        "3",
+        "3",
+        "2",
+        "3",
+        "4",
+        "1",
+        "3",
+        "3"
+        ]
+        }
 ]
 module.exports = friendsArray;
